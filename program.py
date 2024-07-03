bde3b37013485ea4c2c3fdc2347448901899d366
pking23 <pking23@gmu.edu>
2024-07-03 14:57:34 -0400

    feat: Add function to run diarization and audio processing, and generate output with speaker labels

---
+++ program.py
@@ -0,0 +1,9 @@
+## Run diarization.py and then run audio.py and match the output timings of both to create a new with the speaker labels with the transcription output line by line
+
+## TODO: Run diarization.py as a function
+
+## TODO: Run audio.py as a function
+
+## TODO: Match the output of both functions line by line
+
+## TODO: Open a new txt file and write the output of both functions line by line
