9908c57e2599fa605dc64ed6ecc0ea932b02fa7d
pking23 <pking23@gmu.edu>
2024-07-03 15:34:31 -0400

    Add onnxruntime import for diarization.py and started the logic for merging both diarization and transcription

--- program.py
+++ program.py
@@ -6,4 +6,24 @@
 
 ## TODO: Match the output of both functions line by line
 
+
+time_range_text_dict = {}
+# Loop over each turn (segment) in the diarization to fill in values in the dictionary
+for turn in diarization:
+    # Get the speaker label
+    speaker = turn.track("speaker").label()
+    # Get the start and end time of the turn
+    start = turn.start
+    end = turn.end
+
+    # Map turn.start value to the closest value in our transcription object
+    
+
+    # Concatanate the start time and end time to create a range
+    time_range = f"{start} - {end}"
+    time_range_text_dict[time_range] = [speaker, text]
+
+
+
+
 ## TODO: Open a new txt file and write the output of both functions line by line
