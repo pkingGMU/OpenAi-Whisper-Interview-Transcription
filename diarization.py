e21dd6024b764c0f0bda8c769a739b19d5ad84bb
pking23 <pking23@gmu.edu>
2024-07-03 15:01:20 -0400

    feat: Add diarization functionality and print results

--- diarization.py
+++ diarization.py
@@ -1,7 +1,14 @@
+# Imports
 from pyannote.audio import Pipeline
+
+
+# Load the diarization model
 pipeline = Pipeline.from_pretrained("config.yaml")
+# Load the audio file and run the pipeline
 diarization = pipeline("test_audio.wav")
 
+print (f'{diarization}')
 
+# Print to terminal for testing
 for turn, _, speaker in diarization.itertracks(yield_label=True):
     print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
