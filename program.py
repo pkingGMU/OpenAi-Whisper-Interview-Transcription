f4e985d21001c1ffbb9050670d5f0b2b752d44c2
pking23 <pking23@gmu.edu>
2024-07-08 12:47:03 -0400

    Updated program.py to allow the user to type  in the id and it will find it in a directory. TODO later: Have it search for the folder of the id and pull the audio file

--- program.py
+++ program.py
@@ -1,5 +1,33 @@
 ## Run diarization.py and then run audio.py and match the output timings of both to create a new with the speaker labels with the transcription output line by line
 
+# Imports
+import os
+
+
+
+## TODO: Some easy way to select files without using a file explorer
+# Have user input the file name and get the path from the input
+# Ask user for patient ID
+patient_id = input('Enter the name of the patient: ') + '.wav'
+
+# Get the path of the file by searching a specific directory
+## TODO: Change root folder to cyberduck
+root = r"/Users/patrick/Git Hub/Deep-Speech-Mac-Test/"
+
+for file in os.listdir(root):
+    
+
+    print(file)
+
+    if file == patient_id:
+        file_path = os.path.join(root, file)
+        break
+    else:
+        file_path = 'Didnt find the file'
+
+print (file_path)
+
+
 ## TODO: Run diarization.py as a function
 
 ## TODO: Run audio.py as a function
@@ -17,7 +45,7 @@ for turn in diarization:
     end = turn.end
 
     # Map turn.start value to the closest value in our transcription object
-    
+
 
     # Concatanate the start time and end time to create a range
     time_range = f"{start} - {end}"
