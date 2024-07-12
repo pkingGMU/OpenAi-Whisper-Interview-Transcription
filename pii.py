1f898aec1821947ca548ff757514f8599cd1baaf
Isaacam7 <iamouzou@gmu.edu>
2024-07-12 13:24:59 -0400

    Made combine_diar_transcript2 to combine diarization without skipping a transciption segment

--- pii.py
+++ pii.py
@@ -12,17 +12,28 @@ def remove_pii(txt_file):
     #with open(txt_file, 'r') as file:
     #    text = file.read()
 
-    text = 'My name is John Doe and I live in New York. My phone number is 123-456-7890 and my email is bruh@gmail.com'
+    text = """
+    He is John Doe, who lives at 1234 Elm Street, Springfield, USA, is a software engineer at TechCorp Inc. His email address is john.doe@example.com, and his phone number is 555-123-4567. John’s date of birth is April 15, 1985, and his social security number is 987-65-4321. In his free time, John enjoys hiking and photography. He frequently shops at SuperMart and often uses his credit card ending in 1234 for purchases.
+    """
 
     with open("pii_model.pkl", "rb") as f:
         model = pk.load(f)
 
     to_replace = model(text)
-    word_list = text.split(' ')
 
-    print(to_replace)
-    print(word_list)
+    replacement_dict = {}
+    punctuation = [",","-",".","?","!"]
+    text = text.lower()
 
-    return True
 
-remove_pii("")
+    for replacement in to_replace:
+        if replacement["word"] in punctuation:
+            continue
+        replacement_dict[replacement["word"]] = replacement["entity"].split("-")[1]
+
+    for key, value in replacement_dict.items():
+        text = text.replace(key,value)
+
+    return text
+
+print(remove_pii(""))
